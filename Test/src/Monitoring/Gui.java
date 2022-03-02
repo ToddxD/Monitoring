76531b8e8d405a3b2e568cf7a93b5c9697a41ea8
toad9 <toad9@10.14.115.69>
2022-03-02 12:04:19 +0100

    versuch zu mergen

--- Test/src/Monitoring/Gui.java
+++ Test/src/Monitoring/Gui.java
@@ -1,51 +1,43 @@
 package Monitoring;
 
 import java.awt.Color;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.concurrent.TimeUnit;
-import javax.swing.*;  
+import javax.swing.*;
 
 public class Gui {
 
-	
-	
-	public  void name() {
+	public void name() {
 
-		JFrame f=new JFrame("Herr Bert Monitoring");//creating instance of JFrame  
-		
-		
-		Color c1 = new Color(132,195,190);//RGB Color Code
-		f.getContentPane().setBackground(c1);//Sets the background color to the one saved in "c1"
-		ImageIcon img = new ImageIcon("E:/Herrbert.png");//Sets the Path for the icon "img"
-		f.setIconImage(img.getImage());//Sets the Frame icon to the one saved in "img"
-		
-		
+		JFrame f = new JFrame("Herr Bert Monitoring");// creating instance of JFrame
+
+		Color c1 = new Color(132, 195, 190);// RGB Color Code
+		f.getContentPane().setBackground(c1);// Sets the background color to the one saved in "c1"
+		ImageIcon img = new ImageIcon("E:/Herrbert.png");// Sets the Path for the icon "img"
+		f.setIconImage(img.getImage());// Sets the Frame icon to the one saved in "img"
+
 		JLabel l1 = new JLabel("Anzahl Prozesse");
-		l1.setBounds(30,10,100,40);//x axis, y axis, width, height  
+		l1.setBounds(30, 10, 100, 40);// x axis, y axis, width, height
 		l1.setVisible(true);
-		
+
 		JTextArea t1 = new JTextArea();
-		t1.setBounds(30,100,100, 40);//x axis, y axis, width, height  
+		t1.setBounds(30, 100, 100, 40);// x axis, y axis, width, height
 		t1.setEditable(false);
 		t1.setText("test");
-		
-		JButton b1=new JButton("click");//creating instance of JButton  
-		b1.setBounds(130,100,100, 40);//x axis, y axis, width, height  
-		
-		
-		
+
+		JButton b1 = new JButton("click");// creating instance of JButton
+		b1.setBounds(130, 100, 100, 40);// x axis, y axis, width, height
+
 		JLabel l2 = new JLabel("Maximaler Arbeitsspeicher");
-		l2.setBounds(30,210,200,40);//x axis, y axis, width, height  
+		l2.setBounds(30, 210, 200, 40);// x axis, y axis, width, height
 		l2.setVisible(true);
-		
+
 		JTextArea t2 = new JTextArea();
-		t2.setBounds(30,300,100, 40);//x axis, y axis, width, height  
+		t2.setBounds(30, 300, 100, 40);// x axis, y axis, width, height
 		t2.setEditable(false);
 		t2.setText("test");
-		
-		JButton b2=new JButton("click");//creating instance of JButton  
-		b2.setBounds(130,300,100, 40);//x axis, y axis, width, height  
+
+		JButton b2 = new JButton("click");// creating instance of JButton
+		b2.setBounds(130, 300, 100, 40);// x axis, y axis, width, height
 //		b2.addActionListener(new ActionListener() {
 //	        @Override
 //	        public void actionPerformed(ActionEvent arg0) {
@@ -53,38 +45,32 @@ public class Gui {
 //	        }
 //	    });
 
-		
-	
-		
 		f.add(t1);
 		f.add(t2);
 		f.add(l1);
 		f.add(l2);
-		f.add(b1);//adding button in JFrame  
-		f.add(b2);//adding button in JFrame  
-		
-		f.setSize(400,500);//400 width and 500 height  
-		f.setLayout(null);//using no layout managers  
-		f.setVisible(true);//making the frame visible  
-		
-		
-		int abc = 0;
-		Mailoutput test =	new Mailoutput();
+		f.add(b1);// adding button in JFrame
+		f.add(b2);// adding button in JFrame
+
+		f.setSize(400, 500);// 400 width and 500 height
+		f.setLayout(null);// using no layout managers
+		f.setVisible(true);// making the frame visible
+
+		MysqlCon connector = new MysqlCon();
+
 		while (true) {
-			
-			t1.setText(String.valueOf(abc));
-				t2.setText(String.valueOf(abc));
-		abc++;
-		try {
-			TimeUnit.SECONDS.sleep(5);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			System.err.println("yo ich kann nicht schlafen. Manno ");
-		}
-	
-		
+			connector.connect();
+			t1.setText(connector.getArbeitsspeicher());
+			t2.setText(connector.getProzess());
+			try {
+				TimeUnit.SECONDS.sleep(5);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.err.println("yo ich kann nicht schlafen. Manno ");
+			}
+
 		}
 	}
-	
-}  
+
+}
--- Test/src/Monitoring/MysqlCon.java
+++ Test/src/Monitoring/MysqlCon.java
@@ -1,51 +1,44 @@
 package Monitoring;
 
-import java.sql.*;  
+import java.sql.*;
 
-class MysqlCon{  
-	
-	String prozess = "",
-	 arbeitsspeicher ="";
-	
-	
-	public MysqlCon() {
-		// TODO Auto-generated constructor stub
-	}
-	
-	private void name() {
-		
+class MysqlCon {
 
-	
- 
-	try{  
-		Class.forName("com.mysql.jdbc.Driver");  
-		
-		Connection con=DriverManager.getConnection( 
-				
-				
-				
-		"jdbc:mysql://17.28.163.3:3306/logger","root");//here 17.28.163.3:3306 is host with port, logger is database name and root is username  
-		Statement stmt=con.createStatement(); 
-		
-		ResultSet rs=stmt.executeQuery('SELECT Datum, Prozesse, Arbeitsspeicher FROM Monitoring WHERE Datum = (SELECT max(Datum) FROM Monitoring)'); 
+	String prozess = "", arbeitsspeicher = "";
 
-		this.arbeitsspeicher= rs[0];
-		 this.prozess = rs[1];
-	
-		while()  
-		Prozesse = rs.getString(1)//je nachdem wie gezählt wird eventuell 2
-		Arbeitsspeicher = rs.getString(2)//je nachdem wie gezählt wird eventuell 3
-		System.out.println(rs.getString(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  //Test der einzelnen Werte
-		con.close();  
+	public void connect() {
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			Connection con = DriverManager.getConnection(
+					"jdbc:mysql://17.28.163.3:3306/logger", "root", "");// here 17.28.163.3:3306 is host with port,
+																		// logger is database name and root is username
+			Statement stmt = con.createStatement();
+
+			ResultSet rs = stmt.executeQuery(
+					"SELECT Datum, Prozesse, Arbeitsspeicher FROM Monitoring WHERE Datum = (SELECT max(Datum) FROM Monitoring)");
+
+			prozess = rs.getString(1);// je nachdem wie gezählt wird eventuell 2
+			arbeitsspeicher = rs.getString(2);// je nachdem wie gezählt wird eventuell 3
+			System.out.println(rs.getString(1) + "  " + rs.getString(2) + "  " + rs.getString(3)); // Test der einzelnen
+																									// Werte
+			con.close();
 		}
-	
-	
-	catch(Exception e){ System.out.println(e);
-	
-	
-	}  
-	
+
+		catch (Exception e) {
+			System.out.println(e);
+
+		}
+
 	}
-	
-		  
+
+	public String getProzess() {
+		return prozess;
+	}
+
+	public String getArbeitsspeicher() {
+		return arbeitsspeicher;
+	}
+
 }
